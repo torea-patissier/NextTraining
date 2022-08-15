9ed6db51a562d816cfa9449be04160e8b9627daa
Toréa Patissier <torea.patissier@laplateforme.io>
2022-08-15 02:17:18 +0200

    navigatingProgrammatically

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/product');
+  }
+
   return(
     <>
     <h1>Home page</h1>
-    <Link href='blog'><a >Go to blog</a></Link>
-    <br/>
-    <Link href='product'><a >Go to product</a></Link>
+    <button onClick={handleClick}>Go to product with this button</button>
     </>
   )
 };
+/**
+ * Navigating Programmatically
+ */
