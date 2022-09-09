08a33fd7b7e355cf2c75db43cc7aa3592c6da379
Toréa Patissier <torea.patissier@laplateforme.io>
2022-09-09 09:31:03 +0200

    merge main

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,13 @@ export default function Home() {
   return(
     <>
     <h1>Home page for next JS</h1>
+    <h1>Home page for next JS</h1>
+    <h1>Home page for next JS</h1>
+    <h1>Home page for next JS</h1>
+    <h1>Home page for next JS</h1>
+    <h1>Home page for next JS</h1>
+    <h1>Home page for next JS</h1>
+    <h1>Home page for next JS</h1>
     <Link href="/posts">
     <a>Go to Posts</a>
     </Link>
