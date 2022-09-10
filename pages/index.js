1caa11291872c421266fe29f38af29d2a6c2a81b
Toréa Patissier <torea.patissier@laplateforme.io>
2022-09-10 11:29:57 +0200

    test rebase step1

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     <h1>Home page for next JS</h1>
     <h1>Home page for next JS</h1>
     <h1>Test rebase</h1>
+    <h1>Test rebase</h1>
 
     <Link href="/posts">
     <a>Go to Posts</a>
@@ -14,7 +15,3 @@ export default function Home() {
     </>
   )
 };
-
-/**
- * testMerge
- */
